b200a718b82706b7a2e3a467d8060fd4a56e4696
brennencd <111029254+brennenCdickerson@users.noreply.github.com>
2024-08-15 16:38:17 -0700

    fix len < 90 line

--- benford-first-two-digits/benford_first_digits.py
+++ benford-first-two-digits/benford_first_digits.py
@@ -12,7 +12,7 @@ benford_frequencies = np.log10(1 + (1 / first_two_digits))
 
 # Define path to target data
 root_dir = Path(__file__).parent.parent
-file_name = root_dir / "data" / "dc_subset.csv"
+file_name = root_dir / "data" / "purchasing_card_2023.csv"
 
 # Initial read of purchasing card transaction amounts, ignoring unused columns
 purchases_df = pd.read_csv(file_name, usecols=["TRANSACTION_AMOUNT"])
@@ -21,12 +21,28 @@ purchases_df = pd.read_csv(file_name, usecols=["TRANSACTION_AMOUNT"])
 purchases_df = purchases_df.map(extract_first_digits)
 
 # Count occurences of first two digit combinations and convert to proportion of the total count
-actual_proportions = purchases_df.value_counts(normalize=True)
+actual_proportions = purchases_df["TRANSACTION_AMOUNT"].value_counts(normalize=True)
 
-# If any specific first two digit combinations are missing from the data, create a series with the digit combo as index and 0 as the proportion.
-missing_proportions = create_missing_values(first_two_digits, actual_proportions)
+# If any specific first two digit combinations are missing from the data, 
+# create a series with the digit combo as index and 0 as the proportion.
 
-print(missing_proportions)
+if len(actual_proportions < 90):
+    missing_proportions = create_missing_values(first_two_digits, actual_proportions)
+    complete_actual_proportions = pd.concat([actual_proportions, missing_proportions]).sort_index()
+
+d = {"FIRST_TWO_DIGITS": first_two_digits, "EXPECTED_FREQUENCIES": benford_frequencies,
+                 "ACTUAL_FREQUENCIES": complete_actual_proportions if complete_actual_proportions.any() else actual_proportions}
+
+benford_df = pd.DataFrame(data=d)
+benford_df["ABSOLUTE_DEVIATION"] = (abs(benford_df["EXPECTED_FREQUENCIES"] - benford_df["ACTUAL_FREQUENCIES"]))
+mean_absolute_deviation = benford_df["ABSOLUTE_DEVIATION"].mean()
+
+
+
+print(benford_df.head())
+print(mean_absolute_deviation)
+benford_df.to_csv("test.csv")
+
 
 '''plt.plot(first_two_digits, benford_frequencies)
 plt.xticks(np.arange(10, 91, 10))
