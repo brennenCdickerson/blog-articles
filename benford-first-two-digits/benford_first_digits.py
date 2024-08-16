7042bcff2f1329275154e7c8e2b4322292d3d54b
brennencd <111029254+brennenCdickerson@users.noreply.github.com>
2024-08-16 14:04:08 -0700

    basic functionality complete

--- benford-first-two-digits/benford_first_digits.py
+++ benford-first-two-digits/benford_first_digits.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
-from utils import extract_first_digits, create_missing_values
+from utils import extract_first_digits, create_missing_values, create_graph
 
 
 # Create list of all possible first two digit combinations from 10 to 99
@@ -11,22 +11,20 @@ first_two_digits = np.arange(10, 100)
 # Create list of expected Benford's Law frequencies for each first two digit combination
 benford_frequencies = np.log10(1 + (1 / first_two_digits))
 
-# Define path to target data
+# Define path to target data. Pathlib was familiar, but if this becomes more than a proof of concept then there's no need for a library to do this.
 root_dir = Path(__file__).parent.parent
 file_name = root_dir / "data" / "purchasing_card_2023.csv"
 
-# Initial read of purchasing card transaction amounts, ignoring unused columns
+# Initial read of purchasing card transaction amounts, only using financial amounts
 purchases_df = pd.read_csv(file_name, usecols=["TRANSACTION_AMOUNT"])
 
-# Extracts first two digits from transaction amounts
+# Extract first two digits from transaction amounts
 purchases_df = purchases_df.map(extract_first_digits)
 
-# Count occurences of first two digit combinations and convert to proportion of the total count
+# Count occurences of first two digit combinations and convert to a proportion of the total count
 actual_proportions = purchases_df["TRANSACTION_AMOUNT"].value_counts(normalize=True)
 
-# If any specific first two digit combinations are missing from the data, 
-# create a series with the digit combo as index and 0 as the proportion.
-
+# Check if any first two digit combinations are missing and fill empty amounts with zero.
 if actual_proportions.size < 90:
     missing_proportions = create_missing_values(first_two_digits, actual_proportions)
     complete_actual_proportions = pd.concat([actual_proportions, missing_proportions]).sort_index()
@@ -36,18 +34,12 @@ else:
     d = {"FIRST_TWO_DIGITS": first_two_digits, "EXPECTED_FREQUENCIES": benford_frequencies,
                  "ACTUAL_FREQUENCIES": actual_proportions.sort_index()}
 
+# Create dataframe from lists / pandas series, create calculated column showing deviation from expected, calculate MAD
 benford_df = pd.DataFrame(data=d)
 benford_df["ABSOLUTE_DEVIATION"] = (abs(benford_df["EXPECTED_FREQUENCIES"] - benford_df["ACTUAL_FREQUENCIES"]))
 mean_absolute_deviation = benford_df["ABSOLUTE_DEVIATION"].mean()
 
+# Create graph with Benford's Law line over histogram of actual occurences
+create_graph(benford_df["FIRST_TWO_DIGITS"], benford_df["EXPECTED_FREQUENCIES"], 
+             benford_df["ACTUAL_FREQUENCIES"], mean_absolute_deviation)
 
-print(benford_df.head())
-print(mean_absolute_deviation)
-benford_df.to_csv("test.csv")
-
-
-'''plt.plot(first_two_digits, benford_frequencies)
-plt.bar(actual_proportions)
-plt.xticks(np.arange(10, 91, 10))
-plt.xlim(left=10)
-plt.show()'''
--- benford-first-two-digits/utils.py
+++ benford-first-two-digits/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def extract_first_digits(data):
@@ -19,3 +20,18 @@ def create_missing_values(full_set, partial_set):
     missing_proportions = np.zeros(len(missing_digit_combinations))
 
     return pd.Series(missing_proportions, missing_digit_combinations)
+
+
+def create_graph(digits, expected, actual, mad, bar_color="silver", line_color="darkblue"):
+    fig, ax = plt.subplots()
+    bins = digits
+    values = actual
+    ax.bar(bins, values, color=bar_color, label="Actual Frequencies")
+    plt.plot(digits, expected, color=line_color, label="Benford's Law Expectation")
+    ax.legend()
+    ax.text(x=55, y=0.035, s=f'MAD: {mad:.4f}')
+    plt.xticks(np.arange(10, 91, 10))
+    plt.xlim(left=9, right=99)
+    plt.show()
+
+    return fig, ax
